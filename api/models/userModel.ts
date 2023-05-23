97b277f82372c4a349a163b4861d140d1cdeeeae
Alexander <alexander.persson@chasacademy.se>
2023-05-23 19:03:33 +0200

    added comments

--- api/models/userModel.ts
+++ api/models/userModel.ts
@@ -1,75 +1,95 @@
-import mongoose, { Document } from "mongoose";
+import mongoose, { ObjectId, Query } from "mongoose";
+import { UserDocument } from "../types/mongooseTypes";
 import validator from "validator";
 import bcrypt from "bcryptjs";
 
-interface UserDocument extends Document {
-	name: string;
-	email: string;
-	password: string;
-	passwordConfirm?: string;
-	agreeTerms: boolean;
-	role: string;
-	comparePasswords: (candidatePassword: string) => Promise<boolean>;
-}
+const userSchema = new mongoose.Schema<UserDocument>(
+    {
+        name: {
+            type: String,
+            required: [true, "Name is required."],
+            unique: true,
+            minlength: [3, "Name must be at least 3 characters long."],
+            maxlength: [50, "Name cannot be more than 50 characters long."],
+        },
+        email: {
+            type: String,
+            required: [true, "Email is required."],
+            lowercase: true,
+            unique: true,
+            validate: [validator.isEmail, "Please provide a valid email!"],
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required."],
+            minlength: [8, "Password must be at least 8 characters long"],
+            match: [
+                /^(?=.*[!@#$%^&*])/,
+                "Password must contain at least one special character (!@#$%^&*)",
+            ],
+            select: false, //Does not read it when getting the data from the DB
+        },
+        passwordConfirm: {
+            type: String, // set type to String (fix)
+            required: [true, "PasswordConfirm is required."],
+            validate: {
+                validator: function (this: UserDocument, pass: string): boolean {
+                    return pass === this.password;
+                },
+            },
+        },
+        agreeTerms: {
+            type: Boolean,
+            required: [
+                true,
+                "Please agree to the terms and conditions to use this service!",
+            ],
+            validate: {
+                validator: function(v: boolean): boolean {
+                    return v === true;
+                },
+                message: "You must agree to the terms and conditions!"
+            },
+        },
+        role: {
+            type: String,
+            enum: ["admin", "user"],
+            default: "user",
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
 
-const userSchema = new mongoose.Schema<UserDocument>({
-	name: {
-		type: String,
-		required: [true, "Name is required."],
-		unique: true,
-	},
-	email: {
-		type: String,
-		required: [true, "Email is required."],
-		lowercase: true,
-		unique: true,
-		validate: [validator.isEmail, "Please provide a valid email!"],
-	},
-	password: {
-		type: String,
-		required: [true, "Password is required."],
-		minlength: [8, "Password must be at least 8 characters long"],
-		match: [
-			/^(?=.*[!@#$%^&*])/,
-			"Password must contain at least one special character (!@#$%^&*)",
-		],
-		select: false,
-	},
-	passwordConfirm: {
-		type: String,
-		required: [true, "Please confirm your password."],
-		validate: {
-			validator: function(this: UserDocument, el: string): boolean {
-				// only works on CREATE and SAVE
-				return el === this.password;
-			},
-			message: 'Passwords are not the same!'
-		}
-	},
-	agreeTerms: {
-		type: Boolean,
-		required: [true, "Please agree to the terms and conditions to use this service!"],
-		validate: [(v: boolean) => v, "You must agree to the terms and conditions!"],
-	},
-	role: {
-		type: String,
-		enum: ["admin", "user"],
-		default: "user",
-	},
-}, {
-	timestamps: true,
+//Before "save", hash the password with bcrypt
+userSchema.pre("save", async function (next) {
+    //TODO: add only run if password has been modified
+    if (!this.isModified("password")) {
+        return next();
+    }
+
+    this.password = await bcrypt.hash(this.password, 12);
+
+    //After the password it compared and verified, remove the plaintext "confirm password"
+    this.passwordConfirm = undefined;
+    next();
 });
 
-userSchema.pre('save', async function(next) {
-	if (!this.isModified('password')) return next();
+// Funkar ej pga typ  :((((((
+/* userSchema.post("find", function (next) {
+    this._id = this._id.toString();
+    next();
+}); */
 
-	this.password = await bcrypt.hash(this.password, 12);
-	this.passwordConfirm = undefined;
-	next();
-});
-
-userSchema.methods.comparePasswords = async function(candidatePassword: string) {
-	return await bcrypt.compare(candidatePassword, this.password);
+userSchema.methods.comparePasswords = async function (
+    currentPassword: string,
+    originalPassword: string
+) {
+    return await bcrypt.compare(currentPassword, originalPassword);
 };
 
-export default mongoose.model<UserDocument>("User", userSchema);
+// type User = mongoose.InferSchemaType<typeof userSchema>;
+
+const User = mongoose.model("User", userSchema);
+export default User;
