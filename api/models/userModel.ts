a6869e723329d48230b84b7ea8811f1c2cc2bc04
Alexander <alexander.persson@chasacademy.se>
2023-05-23 13:53:34 +0200

    validation fix

--- api/models/userModel.ts
+++ api/models/userModel.ts
@@ -1,94 +1,75 @@
-import mongoose, { ObjectId, Query } from "mongoose";
-import { UserDocument } from "../types/mongooseTypes";
+import mongoose, { Document } from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
 
-const userSchema = new mongoose.Schema<UserDocument>(
-	{
-		name: {
-			type: String,
-			required: [true, "Name is required."],
-			unique: true,
-		},
-		email: {
-			type: String,
-			required: [true, "Email is required."],
-			lowercase: true,
-			unique: true,
-			validate: [validator.isEmail, "Please provide a valid email!"],
-		},
-		password: {
-			type: String,
-			required: [true, "Password is required."],
-			minlength: [8, "Password must be at least 8 characters long"],
-			match: [
-				/^(?=.*[!@#$%^&*])/,
-				"Password must contain at least one special character (!@#$%^&*)",
-			],
-			select: false, //Does not read it when getting the data from the DB
-		},
-		passwordConfirm: {
-			type: {}, //set 'any' type FIXME!
-			required: [true, "PasswordConfirm is required."],
+interface UserDocument extends Document {
+	name: string;
+	email: string;
+	password: string;
+	passwordConfirm?: string;
+	agreeTerms: boolean;
+	role: string;
+	comparePasswords: (candidatePassword: string) => Promise<boolean>;
+}
 
-			validate: {
-				validator: function (this: UserDocument, pass: string): boolean {
-					return pass === this.password;
-				},
-			},
-		},
-		agreeTerms: {
-			type: Boolean,
-			required: [
-				true,
-				"Please agree to the terms and conditions to use this service!",
-			],
-			validate: {
-				validator: function(v: boolean): boolean {
-					return v === true;
-				},
-				message: "You must agree to the terms and conditions!"
-			},
-		},
-		role: {
-			type: String,
-			enum: ["admin", "user"],
-			default: "user",
-		},
+const userSchema = new mongoose.Schema<UserDocument>({
+	name: {
+		type: String,
+		required: [true, "Name is required."],
+		unique: true,
 	},
-	{
-		timestamps: true,
-	}
-);
+	email: {
+		type: String,
+		required: [true, "Email is required."],
+		lowercase: true,
+		unique: true,
+		validate: [validator.isEmail, "Please provide a valid email!"],
+	},
+	password: {
+		type: String,
+		required: [true, "Password is required."],
+		minlength: [8, "Password must be at least 8 characters long"],
+		match: [
+			/^(?=.*[!@#$%^&*])/,
+			"Password must contain at least one special character (!@#$%^&*)",
+		],
+		select: false,
+	},
+	passwordConfirm: {
+		type: String,
+		required: [true, "Please confirm your password."],
+		validate: {
+			validator: function(this: UserDocument, el: string): boolean {
+				// only works on CREATE and SAVE
+				return el === this.password;
+			},
+			message: 'Passwords are not the same!'
+		}
+	},
+	agreeTerms: {
+		type: Boolean,
+		required: [true, "Please agree to the terms and conditions to use this service!"],
+		validate: [(v: boolean) => v, "You must agree to the terms and conditions!"],
+	},
+	role: {
+		type: String,
+		enum: ["admin", "user"],
+		default: "user",
+	},
+}, {
+	timestamps: true,
+});
 
-//Before "save", hash the password with bcrypt
-userSchema.pre("save", async function (next) {
-	//TODO: add only run if password has been modified
-	if (!this.isModified("password")) {
-		return next();
-	}
+userSchema.pre('save', async function(next) {
+	if (!this.isModified('password')) return next();
 
 	this.password = await bcrypt.hash(this.password, 12);
-
-	//After the password it compared and verified, remove the plaintext "confirm password"
 	this.passwordConfirm = undefined;
 	next();
 });
 
-// Funkar ej pga typ  :((((((
-/* userSchema.post("find", function (next) {
-	this._id = this._id.toString();
-	next();
-}); */
-
-userSchema.methods.comparePasswords = async function (
-	currentPassword: string,
-	originalPassword: string
-) {
-	return await bcrypt.compare(currentPassword, originalPassword);
+userSchema.methods.comparePasswords = async function(candidatePassword: string) {
+	return await bcrypt.compare(candidatePassword, this.password);
 };
 
-// type User = mongoose.InferSchemaType<typeof userSchema>;
-
-const User = mongoose.model("User", userSchema);
-export default User;
+export default mongoose.model<UserDocument>("User", userSchema);
