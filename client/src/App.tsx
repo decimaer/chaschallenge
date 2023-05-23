c49facc8ab6ec91146f0cd3d7a756e4c04b56b9f
Tobias-ChasStudent <114392434+Tobias-ChasStudent@users.noreply.github.com>
2023-05-23 10:20:29 +0200

    Added dummy Routes and Components for About, Contact, Rules, Tips and Profile

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,11 @@ import './App.css'
 import Home from './features/LandingPage/Home'
 import Register from './features/User/register/Register';
 import Login from './features/User/login/Login';
+import Tips from './features/Misc/tips/Tips';
+import Contact from './features/Misc/contact/Contact';
+import Profile from './features/User/profile/Profile';
+import Rules from './features/Misc/rules/Rules';
+import About from './features/Misc/about/About';
 import { UserContext } from './state/context';
 
 function App() {
@@ -19,11 +24,11 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-      {/*         <Route path="/contact" element={<Results />} />
-          <Route path="/tips" element={<Results />} />
-          <Route path="/profile" element={<Results />} />
-          <Route path="/rules" element={<Results />} />
-          <Route path="/about" element={<Results />} /> */}
+              <Route path="/contact" element={<Contact />} />
+          <Route path="/tips" element={<Tips />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/rules" element={<Rules />} />
+          <Route path="/about" element={<About />} />
           {/* Error page   */}
           <Route path="*" element={<h1>Error 404: Page not found!</h1>} />
         </Routes>
