13508d50507209ab9cccef92690d567305e27c91
Joel Sellgren <joel.sellgren@hotmail.com>
2023-05-25 08:30:47 +0200

    installed and started setting up default fonts

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        josefin: ['Josefin Sans', 'sans-serif'],
+        manrope: ['Manrope', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
